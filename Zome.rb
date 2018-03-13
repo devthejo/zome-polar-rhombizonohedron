395f2af417631e2807f1052cb02db5c2b7b1d532
takion <jo@surikat.pro>
2018-03-13 12:43:07 +0100

    warehouse wip

--- Zome.rb
+++ Zome.rb
@@ -1,9 +1,13 @@
 require 'sketchup.rb'
 require 'extensions.rb'
 
-zome_extension = SketchupExtension.new('Zome', 'Zome/core.rb')
-zome_extension.version = '1.2.4'
-zome_extension.copyright = '2018'
-zome_extension.description = 'Zome Creator - PolarZonahedron'
-zome_extension.creator = 'Jo Takion <jo@redcat.ninja>'
-Sketchup.register_extension(zome_extension, true)
+module Takion
+  module Zome
+    zome_extension = SketchupExtension.new('Zome', 'Zome/core.rb')
+    zome_extension.version = '1.2.4'
+    zome_extension.copyright = '2018'
+    zome_extension.description = 'Zome Creator - PolarZonahedron'
+    zome_extension.creator = 'Jo Takion <jo@redcat.ninja>'
+    Sketchup.register_extension(zome_extension, true)
+  end
+end
